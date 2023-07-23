9a301595dfb31791ae30d2af6caaf6049d47a8c0
abdullahfirdowsi <abdullahtrickings@gmail.com>
2023-07-23 07:24:35 +0530

    main commit

---
+++ React.js/reaact_/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+//import'./stud/stud/style.css'
+import reportWebVitals from './reportWebVitals';
+//import { BrowserRouter } from 'react-router-dom';
+//import App from './stud/stud/student/App';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js";
+//import Main from './stud/stud/admin/navi';
+//import Main from './stud/stud/student/App';
+import Main from './E_learning/Main'
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<>
+<Main/>
+{/* <BrowserRouter> */}
+{/* <App/> */}
+{/* <Main/> */}
+{/* </BrowserRouter> */}
+</>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
